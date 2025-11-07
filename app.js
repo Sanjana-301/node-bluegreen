9508d8f150189bc90828d6ba63166a801ec4c153
Sanjana-301 <sanjana4047@gmail.com>
2025-11-07 22:43:52 +0530

    Add environment variable support to app.js

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const environment = process.env.ENV || "UNKNOWN";
+
 app.get("/", (req, res) => {
-  res.send("Hello from BLUE-GREEN deployment!");
+  res.send(`Hello from ${environment} environment!`);
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port} (${environment})`);
 });
-
-
-
-
-
